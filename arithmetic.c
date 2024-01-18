acf7f29dc6da4e9ecf8707e1bc4d8d38755dd390
thisislaait <ogadi.ugo@gmail.com>
2024-01-18 15:35:08 +0100

    Fix: use stack parameters in check-error

--- arithmetic.c
+++ arithmetic.c
@@ -7,7 +7,7 @@
  */
 void monty_sub(stack_t **stack, unsigned int line_number)
 {
-    if (*stack == NULL || (*stack)->next == NULL)
+    if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
     {
         fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
         exit(EXIT_FAILURE);
@@ -24,7 +24,7 @@ void monty_sub(stack_t **stack, unsigned int line_number)
  */
 void monty_divide(stack_t **stack, unsigned int line_number)
 {
-    if (*stack == NULL || (*stack)->next == NULL)
+    if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
     {
         fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
         exit(EXIT_FAILURE);
@@ -47,7 +47,7 @@ void monty_divide(stack_t **stack, unsigned int line_number)
  */
 void monty_multiply(stack_t **stack, unsigned int line_number)
 {
-    if (*stack == NULL || (*stack)->next == NULL)
+    if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
     {
         fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
         exit(EXIT_FAILURE);
@@ -64,7 +64,7 @@ void monty_multiply(stack_t **stack, unsigned int line_number)
  */
 void monty_moddy(stack_t **stack, unsigned int line_number)
 {
-    if (*stack == NULL || (*stack)->next == NULL)
+    if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
     {
         fprintf(stderr, "L%u: can't mod, stack too short\n", line_number);
         exit(EXIT_FAILURE);
