9ec31cb61523a5cdb05d0a8a0bac83e30dd7e3b0
thisislaait <ogadi.ugo@gmail.com>
2024-01-18 03:47:11 +0100

    Fix: keep script clean to compile

--- pall.c
+++ pall.c
@@ -5,27 +5,17 @@
  * @stack: Double pointer to the beginning of the stack.
  * @line_number: Line number in the Monty bytecodes file.
  */
+
 void pall(stack_t **stack, unsigned int line_number)
 {
+	stack_t *tmp;
+
 	UNUSED(line_number);
 
-	print_stack(*stack);
-}
-
-/**
- * print_stack - Prints all values in the stack.
- * @stack: Pointer to the top of the stack.
- */
-void print_stack(stack_t *stack)
-{
-    UNUSED(stack);
-    stack_t *tmp = stack;
-
-
+	tmp = *stack;
 	while (tmp != NULL)
 	{
 		printf("%d\n", tmp->n);
 		tmp = tmp->next;
 	}
 }
-
--- push.c
+++ push.c
@@ -5,72 +5,48 @@
  * @stack: Double pointer to the beginning of the stack.
  * @line_number: Line number in the Monty bytecodes file.
  */
+
 void push(stack_t **stack, unsigned int line_number)
 {
-	char *push_arg = strtok(NULL, "\n\t");
+	stack_t *new, *tmp;
+	char *push_arg = strtok(NULL, "\n \t");
 	int pVal;
-
-	if (!is_valid_push_arg(push_arg, line_number))
+	/*if push, tests if the push_arg was valid or not */
+	if (!is_int(push_arg))
+	{
+		fprintf(stdout, "L%u: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
+	}
+
 
 	pVal = atoi(push_arg);
-
-	stack_t *new_node = create_node(pVal);
-	if (!new_node)
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
 	{
 		fprintf(stdout, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
-
-	if (*stack == NULL)
-		*stack = new_node;
+	new->n = pVal;
+	new->prev = NULL;
+	new->next = NULL;
+	/** checks if stack is empty **/
+	if ((*stack) == NULL)
+		*stack = new;
 	else if (SQ)
 	{
-		new_node->next = *stack;
-		(*stack)->prev = new_node;
-		*stack = new_node;
+		/** puts new node on top if not empty **/
+		(*stack)->prev = new;
+		new->next = *stack;
+		*stack = new;
 	}
 	else
 	{
-		stack_t *tmp = *stack;
+		/**puts new node on the bottom **/
+		tmp = *stack;
 		while (tmp->next != NULL)
 			tmp = tmp->next;
-		tmp->next = new_node;
-		new_node->prev = tmp;
+		tmp->next = new;
+		new->prev = tmp;
 	}
+
 }
-
-/**
- * is_valid_push_arg - Checks if the argument for push is valid.
- * @push_arg: Argument for push opcode.
- * @line_number: Line number in the Monty bytecodes file.
- * Return: 1 if valid, 0 otherwise.
- */
-int is_valid_push_arg(char *push_arg, unsigned int line_number)
-{
-	if (!push_arg || (!isdigit(*push_arg) && *push_arg != '-'))
-	{
-		fprintf(stdout, "L%u: usage: push integer\n", line_number);
-		return 0;
-	}
-	return 1;
-}
-
-/**
- * create_node - Creates a new stack node.
- * @value: Value to be assigned to the new node.
- * Return: Pointer to the newly created node.
- */
-stack_t *create_node(int value)
-{
-	stack_t *new_node = malloc(sizeof(stack_t));
-	if (!new_node)
-		return NULL;
-
-	new_node->n = value;
-	new_node->prev = NULL;
-	new_node->next = NULL;
-
-	return new_node;
-}
-
