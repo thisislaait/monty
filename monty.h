135e860bf1b52a2e74dbe3388720256ea38640bc
Ezekiel Gwamna <izzymarc@gmail.com>
2024-01-18 18:48:38 +0100

    README.md

--- monty.h
+++ monty.h
@@ -14,7 +14,7 @@
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO Holberton project
+ * for stack, queues, LIFO, FIFO ALXAfrica project
  */
 typedef struct stack_s
 {
